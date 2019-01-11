381ed87e55c355b2af079cb559534216b4185d66
junyuntee1998 <45333256+junyuntee1998@users.noreply.github.com>
2019-01-11 09:07:16 +0800

    Add files via upload

--- website/js/index.js
+++ website/js/index.js
@@ -1,5 +1,10 @@
-$(function(){
-	
+$(document).ready(
+	$('.navbar-toggler-icon').on('click', function(){
+   $(this).toggleClass('on');
+})
+
+	);
+
 	$(window).scroll(function(){
 		if($(window).scrollTop() <= 40){
 			$(".navbar-brand").css("font-size","50px");
@@ -12,4 +17,33 @@ $(function(){
 		}; 
 		
 	});
-});
+var stuff
+var xhr = new XMLHttpRequest();
+xhr.open('GET','content.json',true);
+xhr.responseType='text';
+xhr.send();
+
+xhr.onload=function(){
+	if(xhr.status===200){
+		stuff = JSON.parse(xhr.responseText);
+document.getElementById('title').innerHTML=stuff.title;
+		document.getElementById('title2').innerHTML=stuff.hero_title;
+document.getElementById('cardBody').innerHTML=stuff.article_snippet_1.desc;
+document.getElementById('cardBody2').innerHTML=stuff.article_snippet_2.desc;
+document.getElementById('cardBody3').innerHTML=stuff.article_snippet_3.desc;
+		document.getElementById('cardBody4').innerHTML=stuff.article_snippet_4.desc;
+		document.getElementById('cardBody5').innerHTML=stuff.article_snippet_5.desc;
+		document.getElementById('cardBody6').innerHTML=stuff.article_snippet_6.desc;
+document.getElementById('image').src=stuff.article_snippet_1.img_path;
+document.getElementById('image2').src=stuff.article_snippet_2.img_path;
+document.getElementById('image3').src=stuff.article_snippet_3.img_path;
+		document.getElementById('image4').src=stuff.article_snippet_3.img_path;
+		document.getElementById('image5').src=stuff.article_snippet_3.img_path;
+		document.getElementById('image6').src=stuff.article_snippet_3.img_path;
+		document.getElementById('descTitle').innerHTML=stuff.feature_section_1.title;
+		document.getElementById('number').innerHTML=stuff.feature_section_1.number;
+	}
+	
+}
+	
+
